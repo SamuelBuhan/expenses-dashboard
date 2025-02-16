7b215e0ddeb9a19db8d2d72de9c69d1142ba8bc5
sbuhan <samuelbuhan@gmail.com>
2025-02-15 20:56:43 -0500

    add remove feature for database

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sqlite3
 st.set_page_config(page_title="Expenses Dashboard",
                         layout="wide")
 
-''' Gloabal variable '''
+# Gloabal variable
 NORMAL_SIZE = 1
 INPUT_SIZE = 3
 GRAPH_SIZE = 6
@@ -20,10 +20,8 @@ VALUE_LABEL = "Value"
 
 SQL_DATABASE = "database.db"
 
-'''
-    SQL
 
-'''
+# SQL
 def create_db():
     conn = sqlite3.connect(SQL_DATABASE)  
     cursor = conn.cursor()
@@ -31,6 +29,7 @@ def create_db():
     # Create the correct table if it doesn't exist
     cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS entries (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         {NAME_LABEL} TEXT,
         {DATE_LABEL} TEXT,
         {TYPE_LABEL} TEXT,
@@ -49,15 +48,6 @@ def reset_database():
 
     # Check the number of rows after deletion
     cursor.execute("SELECT COUNT(*) FROM entries")
-    count = cursor.fetchone()[0]
-    st.write(f"Rows after deletion: {count}")
-    # cursor.execute(f'''
-    # CREATE TABLE entries (
-    #     {NAME_LABEL} TEXT,
-    #     {DATE_LABEL} TEXT,
-    #     {TYPE_LABEL} TEXT,
-    #     {VALUE_LABEL} INTEGER)
-    # ''')
 
     conn.commit()
     conn.close()
@@ -65,7 +55,7 @@ def reset_database():
 # Function to connect to database
 def get_db_connection():
     conn = sqlite3.connect(SQL_DATABASE)
-    conn.row_factory = sqlite3.Row
+    #conn.row_factory = sqlite3.Row
     return conn
 
 # Insert data into the database
@@ -75,6 +65,13 @@ def insert_data(name, date, type, number):
     data_to_store = f"{NAME_LABEL}, {DATE_LABEL}, {TYPE_LABEL}, {VALUE_LABEL}"
     cursor.execute(f"INSERT INTO entries ({data_to_store}) VALUES (?, ?, ?, ?)",
                     (name, date, type, number))
+    conn.commit()
+    conn.close()
+
+def remove_entry(entry_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM entries WHERE id=?", (entry_id,))
     conn.commit()
     conn.close()
 
@@ -153,8 +150,8 @@ def input_data_tab():
     # st.divider()
 
     #st.subheader("Create your CSV file")
-    col_name, col_date, col_type, col_number, col_add, col_remove = st.columns([INPUT_SIZE, INPUT_SIZE, INPUT_SIZE,
-                                                                    INPUT_SIZE, NORMAL_SIZE, NORMAL_SIZE])
+    col_name, col_date, col_type, col_number, col_add = st.columns([INPUT_SIZE, INPUT_SIZE, INPUT_SIZE,
+                                                                    INPUT_SIZE, NORMAL_SIZE])
 
     with col_name:
         exp_name = st.text_input("name")
@@ -178,26 +175,31 @@ def input_data_tab():
                 insert_data(exp_name, exp_date, exp_type, exp_number)
             else:
                 st.write("it miss at least one argument for expense")
-    with col_remove:
-         # workaround to align
-        st.write("") 
-        st.write("")
-
-        if st.button("Remove", type='primary', use_container_width=True):
-            reset_database()
-            print("Database cleared successfully.")
-            st.write(f"You remove {exp_name}")  
 
     st.subheader("View here the expense added:")
-    df_data = get_data()
-    if df_data is not None:
-        st.dataframe(df_data)
-        st.download_button(
-            label="Download data as CSV",
-            data=convert_df(df_data),
-            file_name="personal_finance.csv",
-            mime="text/csv",
-        )
+    col_view_data, col_id_rm,_, _= st.columns([INPUT_SIZE, NORMAL_SIZE, NORMAL_SIZE, 
+                                                  NORMAL_SIZE])
+
+    with col_view_data:
+        df_data = get_data()
+        if df_data is not None:
+            st.dataframe(df_data, use_container_width=True)
+            st.download_button(
+                label="Download data as CSV",
+                data=convert_df(df_data),
+                file_name="personal_finance.csv",
+                mime="text/csv",
+            )
+
+    with col_id_rm:
+        df_data = get_data()
+        if df_data is not None:
+            entry_to_remove = st.selectbox("Select an entry to remove", df_data["id"].tolist())
+            if st.button("Remove", type='primary', use_container_width=False):
+                remove_entry(entry_to_remove)
+                st.warning(f"Entry {entry_to_remove} removed!")
+                # rerun all script for update
+                st.rerun()
     
 
 
